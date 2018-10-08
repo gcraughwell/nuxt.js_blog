2fd0981ee7bacca99ba84a1f46b1c7063c03aeba
gcraughwell <garycraughwell@me.com>
2018-10-08 12:26:48 -0400

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Open Sans", "san-serif";
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+const pkg = require("./package");
+
+module.exports = {
+  mode: "universal",
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      //how to add custom fonts
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Open+Sans"
+      }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: "#FFFFFF" },
+
+  /*
+  ** Global CSS
+  */
+  css: [],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {}
+  }
+};
